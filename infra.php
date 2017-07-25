d2135faa51e19c22a6d496ec9fcff11784e024fa
Anton Kiyatkin <axlbant@sent.com>
2017-07-25 22:01:16 +0400

    fix notice

--- infra.php
+++ infra.php
@@ -9,7 +9,10 @@ use infrajs\template\Template;
 
 Env::add('lang', function () {
 	$conf = Config::get('lang');
-	preg_match('/^\w{2}/',$_SERVER['HTTP_ACCEPT_LANGUAGE'], $matches);
+	if (!isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) $accept = 'ru';
+	else  $accept = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+	
+	preg_match('/^\w{2}/',$accept, $matches);
 	switch (strtolower($matches[0])){
 	    case "ru": $lang = "ru"; break; // если русский
 	    case "de": $lang = "de"; break; // если немецкий
